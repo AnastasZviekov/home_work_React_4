b97f85f23aa03705cf9f9673753bc65acf657b16
AnastasZviekov <stas.zviekov@gmail.com>
2024-02-21 16:36:40 +0100

    add_state

--- home_work_4/src/App.jsx
+++ home_work_4/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-        <div>
-   <div> {count}</div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-            Decrement
-        </button>
-        <button onClick={() => setCount((count) => 0)}>
-            Reset
-        </button>
-       </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <div> {count}</div>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    Increment
+                </button>
+                <button onClick={() => setCount((count) => count - 1)}>
+                    Decrement
+                </button>
+                <button onClick={() => setCount((count) => 0)}>
+                    Reset
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
